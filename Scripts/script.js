596be633ec119b8e4550fd263de8bfc749c94837
Hashmat Ibrahimi <hashmat.ibrahimi@lc.cuny.edu>
2019-03-17 13:52:45 -0400

    added input validation

--- Scripts/script.js
+++ Scripts/script.js
@@ -38,6 +38,33 @@ $(document).ready(function(){
 
   $(document).ready(function(){
     $("#submitBtn").click(function(){
-        alert("Successfully Submitted.")
+      var name = document.getElementById('name').value;
+      var email = document.getElementById('email').value;
+      var gender = document.getElementById('gender').value;
+      var number = document.getElementById('number').value;
+      console.log(number)
+      if(name === ''){
+        alert("Please Type You Name In To Contact Use");
+      }
+      else{
+        if(email.indexOf('@') == -1 || email.indexOf('.com') === -1){
+          alert('Please Enter the correct Email format ex: test@test.com')
+        }
+        else{
+          if(gender === ''){
+            alert('Please Pick A Gender');
+          }
+          else{
+            if(number === ''){
+              alert('Please Enter A Contact Number To Reach You')
+            }
+            else{
+              alert("Successfully Submitted.")
+            }
+          }
+        }
+      }
+      
+        
     });
   });
